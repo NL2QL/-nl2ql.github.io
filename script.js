5daf0ea048e7d159c6a2523fb8c4adaa5013e40a
Jolene <jolenechong7@gmail.com>
2024-05-05 14:13:03 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
